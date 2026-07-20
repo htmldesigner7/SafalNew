33f42a57b4424a85a8d53e67d8f251eda1c8c44e
htmldesigner7 <htmldesigner7@gmail.com>
2026-07-20 16:17:38 +0530

    home page resposnvie done

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath: '/safal',
   trailingSlash: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
